c8b3055145f1b9be0e7ccd20ab78662191700562
James Scott <jim.scott@urbanairship.com>
2014-01-31 15:21:23 -0800

    Updates for linter_configs

--- imhotep_foodcritic/plugin.py
+++ imhotep_foodcritic/plugin.py
@@ -12,11 +12,13 @@ class FoodCritic(Tool):
     def invoke(self,
                dirname,
                filenames=set(),
-               config_file=None):
+               linter_configs=set()):
         retval = defaultdict(lambda: defaultdict(list))
         if len(filenames) == 0:
             cmd = "find %s/cookbooks -type d -maxdepth 1 ! -path %s/cookbooks | xargs foodcritic" % (dirname, dirname)
         else:
+            if 'cookbooks' not in filenames:
+                return retval
             filenames = ["%s/%s" % (dirname, "/".join(filename.split('/')[:2])) for filename in filenames]
             filnames = list(set(filenames))
             cmd = "foodcritic %s" % (" ".join(filenames))
